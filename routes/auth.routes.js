cc1c8f9cfbe4ae3db77b94ae0b394130891cdf18
Walter Bernal <wltrbm@gmail.com>
2023-08-24 01:41:24 -0700

    Adition of /auth/verify

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,10 +2,14 @@ const router = require("express").Router()
 const {
   postSignupController,
   postLoginController,
+  getVerifyController,
 } = require("../controllers/auth.controller")
+const { isAuthenticated } = require("../middleware/jwt.middleware")
 
 router.post("/signup", postSignupController)
 
 router.post("/login", postLoginController)
+
+router.get('/verify', isAuthenticated, getVerifyController)
 
 module.exports = router
