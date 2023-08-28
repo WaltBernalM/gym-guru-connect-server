c7299a07c93d8006bc845370d25be0df13c53b5e
Walter Bernal <wltrbm@gmail.com>
2023-08-28 00:09:18 -0700

    tokenVersion validation

--- middleware/isValidTokenVer.js
+++ middleware/isValidTokenVer.js
@@ -3,8 +3,14 @@ const { getCurrentTokenVersion } = require("../controllers/config")
 
 exports.isValidTokenVer = async (req, res, next) => { 
   try {
-    console.log('version of client token:', req.payload.version)
-    console.log('current Token version' ,getCurrentTokenVersion())
+    const tokenVersion = req.payload.version
+    const currentTokenVersion = getCurrentTokenVersion()
+    console.log('version of client token:', tokenVersion)
+    console.log('current Token version', currentTokenVersion)
+    if (tokenVersion !== currentTokenVersion) {
+      console.log('token version mismatch')
+      return res.status(401).json({ message: "Unauthorized (invalid Token version" })
+    }
     next()
   } catch (error) { 
     res.status(500).json({ message: 'Intergal Server Error' })
