88b023e64c284526597434d27bef740e0fce5dba
Walter Bernal <wltrbm@gmail.com>
2023-08-28 00:40:02 -0700

    text to fix cors problem in browser

--- config/index.js
+++ config/index.js
@@ -15,6 +15,8 @@ const cors = require("cors");
 
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
 
+const frontendDomain = process.env.FRONTEND_DOMAIN
+
 // Middleware configuration
 module.exports = (app) => {
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
@@ -24,7 +26,8 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL], // added comma
+      // origin: [FRONTEND_URL], // added comma
+      origin: process.ev.NODE_ENV === 'production' ? new RegExp(`https://([a-z0-9.-]*\\.)?${frontendDomain}$`) : "http://localhost:3000",
       credentials: true, // allow credentials
       methods: 'GET, POST, PUT, PATCH, DELETE', // allowed methods
       allowedHeaders: 'Content-Type, Authorization' // allowed headers
