dd1a61beebf3bf97daeb8a8229c03101cfd348a2
Walter Bernal <wltrbm@gmail.com>
2023-08-28 00:20:42 -0700

    Fixed issue with cookies at browsers cache with versioning of Token

--- middleware/isValidTokenVer.js
+++ middleware/isValidTokenVer.js
@@ -5,10 +5,7 @@ exports.isValidTokenVer = async (req, res, next) => {
   try {
     const tokenVersion = req.payload.version
     const currentTokenVersion = getCurrentTokenVersion()
-    console.log('version of client token:', tokenVersion)
-    console.log('current Token version', currentTokenVersion)
     if (tokenVersion !== currentTokenVersion) {
-      console.log('token version mismatch')
       return res.status(401).json({ message: "Unauthorized (invalid Token version" })
     }
     next()
