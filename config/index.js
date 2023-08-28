f84519de126d53980bfaaa74b36e7eded0d444aa
Walter Bernal <wltrbm@gmail.com>
2023-08-28 01:14:25 -0700

    allowed credentials in cors

--- config/index.js
+++ config/index.js
@@ -25,7 +25,7 @@ module.exports = (app) => {
   app.use(
     cors({
       origin: [FRONTEND_URL], // added comma
-      // credentials: true, // allow credentials
+      credentials: true, // allow credentials
       // methods: 'GET, POST, PUT, PATCH, DELETE', // allowed methods
       // allowedHeaders: 'Content-Type, Authorization' // allowed headers
     })
