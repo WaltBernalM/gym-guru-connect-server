914a32687c41ebf81dfd2339773454bf480029b3
Walter Bernal <wltrbm@gmail.com>
2023-08-28 00:58:03 -0700

    version 2 of jwt.middleware

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -7,13 +7,35 @@ const getTokenFromCookies = (req) => {
   return null
 }
 
-const isAuthenticated = jwt({
-  secret: process.env.SECRET_KEY,
-  algorithms: ["HS256"],
-  requestProperty: "payload",
-  getToken: getTokenFromCookies
-})
+// version 1
+// const isAuthenticated = jwt({
+//   secret: process.env.SECRET_KEY,
+//   algorithms: ["HS256"],
+//   requestProperty: "payload",
+//   getToken: getTokenFromCookies
+// })
 
-module.exports = { isAuthenticated }
+// version 2
+const isAuthenticated = (req, res, next) => {
+  const token = getTokenFromCookies(req)
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized: Missing authToken cookie" })
+  }
+
+  jwt({
+    secret: process.env.SECRET_KEY,
+    algorithms: ["HS256"],
+    requestProperty: "payload",
+    getToken: getTokenFromCookies
+  })(req, res, (error) => {
+    if (error) {
+      return res.status(401).json({message: 'Unauthorized: Invalid authentication Token'})
+    }
+    next()
+  })
+}
 
 module.exports = { isAuthenticated }
