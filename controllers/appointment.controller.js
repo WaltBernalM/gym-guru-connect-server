b306eda1f5bed8bc83ed37689594a4bd416c8b5e
Walter Bernal <wltrbm@gmail.com>
2023-08-30 19:39:32 -0700

    dayInfo tests 2.9

--- controllers/appointment.controller.js
+++ controllers/appointment.controller.js
@@ -40,7 +40,7 @@ const postCreateAppointment = async (req, res, next) => {
     
     const today = new Date(currentDate)
     
-    const todayPlusTwo = new Date(today.setDate(today.getDate() + 3)).toLocaleString("en-US", options)
+    const todayPlusTwo = new Date(today.setDate(today.getDate() + 3))
     console.log(fixedInputDate, todayPlusTwo)
     
     
