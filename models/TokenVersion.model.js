391741b48c45c7b538cdbecb761ceb5269f18a21
Walter Bernal <wltrbm@gmail.com>
2023-08-30 17:32:22 -0700

    creation of model for token versioning

---
+++ models/TokenVersion.model.js
@@ -0,0 +1,14 @@
+const { model, Schema } = require('mongoose')
+
+const tokenVersionSchema = new Schema(
+  {
+    version: {
+      type: Number,
+      default: 1
+    }
+  },
+  { timestamps: true}
+)
+
+const TokenVersion = model("TokenVersion", tokenVersionSchema)
+export default TokenVersion
