4649ee3fd2e73eba7ae6bfcfe966040aa0b87a5e
Walter Bernal <wltrbm@gmail.com>
2023-08-30 17:38:02 -0700

    initial implementation of tokenversionDB

--- middleware/isValidTokenVer.js
+++ middleware/isValidTokenVer.js
@@ -1,11 +1,19 @@
 const { getCurrentTokenVersion } = require("../controllers/config")
+const { default: TokenVersion } = require("../models/TokenVersion.model")
 
 
 exports.isValidTokenVer = async (req, res, next) => { 
   try {
+    const isAnyTokenInDB = await TokenVersion.find()
+    if (!isAnyTokenInDB) {
+      await TokenVersion.create({ version: 1 })
+    }
+
+    const currentTokenVersionInDB = await TokenVersion.find()
+    console.log('dbTokenVer:', currentTokenVersionInDB)
+
     const tokenVersion = req.payload.version
     const currentTokenVersion = getCurrentTokenVersion()
-    console.log('token versions: ', currentTokenVersion, tokenVersion)
     if (tokenVersion !== currentTokenVersion) {
       return res.status(401).json({ message: "Unauthorized (invalid Token version)" })
     }
