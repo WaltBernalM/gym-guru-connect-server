22b3620fb1e6f7bd66501082d20dc196061d8959
Walter Bernal <wltrbm@gmail.com>
2023-10-20 03:51:50 -0700

    Creation of exercise-routines controllers and routes to POST and DELETE

--- routes/exerciseRoutine.routes.js
+++ routes/exerciseRoutine.routes.js
@@ -1,17 +1,37 @@
-const router = require('express').Router()
+"use strict"
+const router = require("express").Router()
 
 const {
   postNewExerciseRoutine,
   putUpdateExerciseRoutine,
   deleteExerciseRoutine,
+  postNewUserRoutine,
+  deleteUserRoutine,
 } = require("../controllers/exerciseRoutine.controller")
-const { selfTraineeOrAllowedTrainer } = require('../middleware/selfTraineeOrAllowedTrainer.middleware')
+const {
+  selfTraineeOrAllowedTrainer,
+} = require("../middleware/selfTraineeOrAllowedTrainer.middleware")
 
 // Adds a new routine to trainee, can be done by trainee or trainer
-router.post('/trainee/:traineeId', selfTraineeOrAllowedTrainer, postNewExerciseRoutine)
+router.post(
+  "/trainee/:traineeId",
+  selfTraineeOrAllowedTrainer,
+  postNewExerciseRoutine
+)
 
-router.put("/:exerciseRoutineId/trainee/:traineeId", selfTraineeOrAllowedTrainer, putUpdateExerciseRoutine)
+router.put(
+  "/:exerciseRoutineId/trainee/:traineeId",
+  selfTraineeOrAllowedTrainer,
+  putUpdateExerciseRoutine
+)
 
-router.delete('/:exerciseRoutineId/trainee/:traineeId', selfTraineeOrAllowedTrainer, deleteExerciseRoutine)
+router.delete(
+  "/:exerciseRoutineId/trainee/:traineeId",
+  selfTraineeOrAllowedTrainer,
+  deleteExerciseRoutine
+)
+
+router.post("/user/:userId", postNewUserRoutine)
+router.delete("/:exerciseRoutineId/user/:userId", deleteUserRoutine)
 
 module.exports = router
